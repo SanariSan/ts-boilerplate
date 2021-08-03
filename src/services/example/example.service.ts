6c4eb7a7d5be506ac13f60f1809de96626e1eb58
SanariSan <76019273+SanariSan@users.noreply.github.com>
2021-08-04 00:47:55 +0300

    fix

--- src/services/example/example.service.ts
+++ src/services/example/example.service.ts
@@ -1,12 +1,11 @@
 import { axiosApiBase, handleRequest } from "./../request-base";
 
-const exampleRequest = ({ arg1 }) => {
+const exampleRequest = ({ arg1 }) =>
 	handleRequest(axiosApiBase.post)({
 		path: "/path",
 		data: {
 			arg1,
 		},
 	});
-};
 
 export { exampleRequest };
